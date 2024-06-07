98ebe4580b003f62294bedeb962e3f5a0d746058
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2024-06-07 08:46:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const { v4: uuidv4 } = require('uuid'); // For generating unique session IDs
-
-const sessions = {}; // Store sessions with IDs
 
 app.use(express.static('public'));
 
@@ -16,35 +13,40 @@ app.get('/listener', (req, res) => {
     res.sendFile(__dirname + '/public/listener.html');
 });
 
+let broadcaster;
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    // Generate a unique session ID
-    const sessionId = uuidv4();
-    sessions[sessionId] = socket.id;
+    socket.on('broadcaster', (stream) => {
+        broadcaster = socket.id;
+        socket.broadcast.emit('broadcaster');
+    });
 
-    // Send session ID to the user
-    socket.emit('sessionID', sessionId);
+    socket.on('offer', (offer) => {
+        if (socket.id !== broadcaster) {
+            socket.to(broadcaster).emit('offer', offer);
+        }
+    });
+
+    socket.on('answer', (answer) => {
+        if (socket.id === broadcaster) {
+            socket.broadcast.emit('answer', answer);
+        }
+    });
+
+    socket.on('candidate', (candidate) => {
+        if (socket.id !== broadcaster) {
+            socket.to(broadcaster).emit('candidate', candidate);
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        // Remove session ID when user disconnects
-        delete sessions[sessionId];
-    });
-
-    socket.on('offer', ({ offer, targetSessionId }) => {
-        // Send offer to the targeted user
-        io.to(sessions[targetSessionId]).emit('offer', { offer, senderSessionId: sessionId });
-    });
-
-    socket.on('answer', ({ answer, senderSessionId }) => {
-        // Send answer to the sender
-        io.to(sessions[senderSessionId]).emit('answer', answer);
-    });
-
-    socket.on('candidate', ({ candidate, targetSessionId }) => {
-        // Send candidate to the targeted user
-        io.to(sessions[targetSessionId]).emit('candidate', { candidate, senderSessionId: sessionId });
+        if (socket.id === broadcaster) {
+            broadcaster = null;
+            socket.broadcast.emit('broadcaster');
+        }
     });
 });
 
